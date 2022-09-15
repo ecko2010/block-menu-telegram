aa203764d10b5b71196953b86ecaef36e3e4e950
ecko2010 <ahmedegy04@gmail.com>
2022-09-15 20:24:52 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,78 +2,78 @@ import telebot
 from telebot import types
 import random
 
-TOKEN = '...'
+TOKEN = '1709981956:AAFuOu2bGcD0CriK8gwjzO6VQVm8i0pO_VI'
 bot = telebot.TeleBot(TOKEN)
 
 
-# Обработчик на команду "старт"
+# Handler for the "start" command
 @bot.message_handler(commands=['start'])
 def start(message):
-    # resize_keybord = True - чтобы размеры кнопок менялись в зависимости от экрана
+    # resize_keybord = True - so that the button sizes change depending on the screen
     markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-    item1 = types.KeyboardButton('Рандомное число')
-    item2 = types.KeyboardButton('Курсы валют')
-    item3 = types.KeyboardButton('Информация')
-    item4 = types.KeyboardButton('Другое')
+    item1 = types.KeyboardButton('random number')
+    item2 = types.KeyboardButton('Exchange rates')
+    item3 = types.KeyboardButton('Information')
+    item4 = types.KeyboardButton('Other')
     markup.add(item1, item2, item3, item4)
 
-    bot.send_message(message.chat.id,'Здравствуй, {0.first_name}!'.format(message.from_user), reply_markup = markup)
+    bot.send_message(message.chat.id,'Hello, {0.first_name}!'.format(message.from_user), reply_markup = markup)
 
 
-# Обработчик на команду "Рандомное число"
+# Handler for the command "Random number"
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
     if message.chat.type == 'private':
-        if message.text == 'Рандомное число':
-            bot.send_message(message.chat.id,'Ваше число: ' + str(random.randint(0,10000)))
+        if message.text == 'random number':
+            bot.send_message(message.chat.id,'your number: ' + str(random.randint(0,10000)))
 
-        # Обработчик на команду "Курсы валют"
-        elif message.text == 'Курсы валют':
+        # Handler for the command "Currency rates"
+        elif message.text == 'Exchange rates':
             markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-            item1 = types.InlineKeyboardButton('USD Курс Доллара')
-            item2 = types.KeyboardButton('EUR Курс Евро')
-            back = types.KeyboardButton('Назад')
+            item1 = types.InlineKeyboardButton('USD exchange rate')
+            item2 = types.KeyboardButton('EUR exchange rate')
+            back = types.KeyboardButton('Back')
             markup.add(item1, item2, back)
 
             bot.send_message(message.chat.id,'Выберите нужную Вам валюту', reply_markup = markup)
 
-        # Обработчик на команду "Информация"
-        elif message.text == 'Информация':
+        # Handler for the "Information" command
+        elif message.text == 'Information':
             markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-            item1 = types.InlineKeyboardButton('Что умеет бот')
-            item2 = types.KeyboardButton('Кто создатель? Миссия?')
-            back = types.KeyboardButton('Назад')
+            item1 = types.InlineKeyboardButton('What can a bot do')
+            item2 = types.KeyboardButton('Who is the creator? Mission?')
+            back = types.KeyboardButton('Back')
             markup.add(item1, item2, back)
 
-            bot.send_message(message.chat.id,'Информация', reply_markup = markup)
+            bot.send_message(message.chat.id,'Information', reply_markup = markup)
 
-        # Обработчик на команду "Другое"
-        elif message.text == 'Другое':
+        # Handler for "Other" command
+        elif message.text == 'Other':
             markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-            item1 = types.InlineKeyboardButton('Настройки')
-            item2 = types.KeyboardButton('Полезные советы')
-            item3 = types.KeyboardButton('Стикер')
-            back = types.KeyboardButton('Назад')
+            item1 = types.InlineKeyboardButton('Settings')
+            item2 = types.KeyboardButton('Helpful Hints')
+            item3 = types.KeyboardButton('Sticker')
+            back = types.KeyboardButton('Back')
             markup.add(item1, item2, item3, back)
 
-            bot.send_message(message.chat.id,'Другое', reply_markup = markup)
+            bot.send_message(message.chat.id,'Other', reply_markup = markup)
         
-        # Кнопка "Назад"
-        elif message.text == 'Назад':
+        # Back button
+        elif message.text == 'Back':
             markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-            item1 = types.KeyboardButton('Рандомное число')
-            item2 = types.KeyboardButton('Курсы валют')
-            item3 = types.KeyboardButton('Информация')
-            item4 = types.KeyboardButton('Другое')
+            item1 = types.KeyboardButton('random number')
+            item2 = types.KeyboardButton('Exchange rates')
+            item3 = types.KeyboardButton('Information')
+            item4 = types.KeyboardButton('Other')
             markup.add(item1, item2, item3, item4)
 
-            bot.send_message(message.chat.id,'Вы вернулись на главное меню', reply_markup = markup)
+            bot.send_message(message.chat.id,'You have returned to the main menu', reply_markup = markup)
 
-        # Кнопка "Стикер"
-        elif message.text == 'Стикер':
+        # Sticker button
+        elif message.text == 'Sticker':
             sticker = open('static/sticker.webp', 'rb')
             bot.send_sticker(message.chat.id, sticker)
 
 
-# Чтобы бот не отключался 
+# So that the bot does not turn off
 bot.polling(none_stop=True)
